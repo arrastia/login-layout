5908945052b3ee757b15875f677e25b37a44c1cc
Arrastia <arrastia@pm.me>
2020-04-10 12:46:29 +0200

    update app styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import styles from "./App.module.scss";
 
 import { Header } from "interfaces/.components/Header";
+import { Login } from "interfaces/.components/Login";
 
 export const App = () => {
   return (
     <div className={styles.App}>
       <Header />
+      <Login />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 
-import { App } from "./App";
+import { App } from "App";
 
 ReactDOM.render(
   <StrictMode>
