284d9b36f5b557df2d5193af3fb38e18b161ed49
Arrastia <arrastia@pm.me>
2020-04-10 19:02:48 +0200

    update app styles

--- src/interfaces/.components/Footer/Footer.jsx
+++ src/interfaces/.components/Footer/Footer.jsx
@@ -1,16 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./Footer.module.scss";
+import { IoIosHeart } from 'react-icons/io';
+
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <a
-        className={styles.link}
-        href="https://arrastia.me"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      Made with <IoIosHeart /> by{' '}
+      <a className={styles.link} href="https://arrastia.me" target="_blank" rel="noopener noreferrer">
         Arrastia
       </a>
     </div>
--- src/interfaces/.components/Login/Login.jsx
+++ src/interfaces/.components/Login/Login.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 
-import styles from "./Login.module.scss";
+import styles from './Login.module.scss';
 
 export const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,54 +13,36 @@ export const Login = () => {
   return (
     <Fragment>
       <div className={styles.logForm}>
-        <div className={`${styles.signup} ${!isLogin ? styles.slideUp : ""}`}>
+        <div className={`${styles.signup} ${!isLogin ? styles.slideUp : ''}`}>
           <h2 className={styles.formTitle} onClick={() => onToggleView()}>
             <span>or</span>Registrate
           </h2>
           <div className={styles.formHolder}>
             <div className={styles.inputWrap}>
-              <input
-                type="text"
-                className={styles.input}
-                placeholder="Nombre"
-              />
+              <input type="text" className={styles.input} placeholder="Nombre" />
             </div>
             <div className={styles.inputWrap}>
-              <input
-                id="logInPass"
-                type="email"
-                className={styles.input}
-                placeholder="Email"
-              />
+              <input id="logInPass" type="email" className={styles.input} placeholder="Email" />
             </div>
             <div className={styles.inputWrap}>
-              <input
-                className={styles.input}
-                id="pass"
-                placeholder="Contraseña"
-                type="password"
-              />
+              <input className={styles.input} id="pass" placeholder="Contraseña" type="password" />
               <i id="eye" className="fa fa-eye showPass"></i>
             </div>
           </div>
           <button className={styles.submitButton}>Registrate</button>
         </div>
-        <div className={`${styles.login} ${isLogin ? styles.slideUp : ""}`}>
+        <div className={`${styles.login} ${isLogin ? styles.slideUp : ''}`}>
           <div className={styles.center}>
             <h2 className={styles.formTitle} onClick={() => onToggleView()}>
               <span>o</span>Inicia sesión
             </h2>
             <div className={styles.formHolder}>
-              <input
-                type="email"
-                className={styles.input}
-                placeholder="Email"
-              />
+              <input type="email" className={styles.input} placeholder="Email" />
               <input
                 className={styles.input}
                 id="pwd"
                 placeholder="Contraseña"
-                type={isPassVisible ? "text" : "password"}
+                type={isPassVisible ? 'text' : 'password'}
               />
             </div>
             <button className={styles.submitButton}>Inicia sesión</button>
